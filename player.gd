937e9fafee48c247482e0849b42111a32f64f502
theadamsmith <12asmith3@gmail.com>
2024-02-09 17:43:25 -0800

    almost done

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ var screen_size
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	#hide()
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -39,3 +39,14 @@ func _process(delta):
 	elif velocity.y != 0:
 		$AnimatedSprite2D.animation = "up"
 		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+func _on_body_entered(body):
+	hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	$CollisionShape2D.set_deferred("disabled", true)
